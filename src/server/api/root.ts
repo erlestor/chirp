02d5df4e5cab6774766c4814f39e349024341372
Erlend Storsve <erlestor@ntnu.no>
2023-03-31 11:10:48 +0200

    add authors profile image

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
-import { createTRPCRouter } from "~/server/api/trpc";
-import { postsRouter } from "./routers/posts";
+import { createTRPCRouter } from '~/server/api/trpc'
+import { postsRouter } from './routers/posts'
 
 /**
  * This is the primary router for your server.
@@ -8,7 +8,7 @@ import { postsRouter } from "./routers/posts";
  */
 export const appRouter = createTRPCRouter({
   posts: postsRouter,
-});
+})
 
 // export type definition of API
-export type AppRouter = typeof appRouter;
+export type AppRouter = typeof appRouter

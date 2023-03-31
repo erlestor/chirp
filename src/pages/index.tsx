322c4b5c1a326580a56d68d745eda387fdb29ffb
Erlend Storsve <erlestor@ntnu.no>
2023-03-31 12:10:07 +0200

    Increase emoji text size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,7 +55,7 @@ const PostView = (props: PostWithUser) => {
           <span>{`@${author.username}`}</span>
           <span className='font-thin'>{` · ${dayjs(post.createdAt).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className='text-2xl'>{post.content}</span>
       </div>
     </div>
   )

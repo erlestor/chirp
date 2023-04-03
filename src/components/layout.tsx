18130767a40b5b331a5ea22e3c114fab7ed83e5a
Erlend Storsve <erlestor@ntnu.no>
2023-04-03 17:41:48 +0200

    Custom scrollbar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren } from "react"
 export const PageLayout = (props: PropsWithChildren) => {
   return (
     <main className="flex justify-center h-screen">
-      <div className="w-full md:max-w-2xl border-x border-slate-400 overflow-y-auto">
+      <div className="w-full md:max-w-2xl border-x border-slate-400 overflow-y-auto scrollbar-thin scrollbar-track-transparent scrollbar-thumb-slate-800">
         {props.children}
       </div>
     </main>

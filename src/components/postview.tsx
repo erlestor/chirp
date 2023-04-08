b25992a996dbcf33156caa005efd683f3d058aba
Erlend Storsve <erlestor@stud.ntnu.no>
2023-04-08 18:23:18 +0200

    Entire post is now a link

--- src/components/postview.tsx
+++ src/components/postview.tsx
@@ -10,29 +10,27 @@ dayjs.extend(relativeTime)
 
 export const PostView = ({ post, author }: PostWithUser) => {
   return (
-    <div key={post.id} className="p-4 border-b border-slate-400 hover:bg-gray-950 flex gap-3">
-      <Link href={`/@${author.username}`}>
-        <Image
-          src={author.profilePicture}
-          alt={`@${author.username}'s profile picture`}
-          className="w-14 h-14 rounded-full"
-          width={56}
-          height={56}
-        />
-      </Link>
-      <div className="flex flex-col grow">
-        <div className="flex text-slate-300 gap-1">
-          <Link href={`/@${author.username}`}>
-            <span>{`@${author.username}`}</span>
-          </Link>
-          <Link href={`/post/${post.id}`}>
-            <span className="font-thin flex grow">{` · ${dayjs(post.createdAt).fromNow()}`}</span>
-          </Link>
-        </div>
-        <Link href={`/post/${post.id}`}>
-          <span className="text-2xl grow">{post.content}</span>
+    <Link href={`/post/${post.id}`}>
+      <div key={post.id} className="p-4 border-b border-slate-400 hover:bg-gray-950 flex gap-3">
+        <Link href={`/@${author.username}`}>
+          <Image
+            src={author.profilePicture}
+            alt={`@${author.username}'s profile picture`}
+            className="w-14 h-14 rounded-full"
+            width={56}
+            height={56}
+          />
         </Link>
+        <div className="flex flex-col grow">
+          <div className="flex text-slate-300 gap-1">
+            <Link href={`/@${author.username}`}>
+              <span>{`@${author.username}`}</span>
+            </Link>
+            <span className="font-thin flex grow">{` · ${dayjs(post.createdAt).fromNow()}`}</span>
+          </div>
+          <span className="text-2xl grow">{post.content}</span>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }

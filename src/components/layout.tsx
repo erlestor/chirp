a42879d0b2e3140a4ff5b1ad216bd80a728dfcd9
Erlend Storsve <erlestor@stud.ntnu.no>
2023-04-09 11:37:22 +0200

    Fix styling on page load

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,20 @@
+import { SignIn, SignInButton, useUser } from "@clerk/nextjs"
 import type { PropsWithChildren } from "react"
 
 export const PageLayout = (props: PropsWithChildren) => {
+  const { isLoaded, isSignedIn } = useUser()
+
   return (
-    <main className="flex justify-center min-h-screen">
-      <div className="w-full md:max-w-2xl border-x border-slate-400">{props.children}</div>
+    <main className="flex flex-col items-center min-h-screen">
+      <div className="w-full md:max-w-2xl border-x border-slate-400 min-h-screen">
+        {props.children}
+      </div>
+      {isLoaded && !isSignedIn && (
+        <div className="absolute bottom-0 p-4 bg-black w-full justify-center flex gap-4">
+          <div className="text-xl">Don't miss what's happening</div>
+          <SignInButton />
+        </div>
+      )}
     </main>
   )
 }

b07c73d0bef6a9250cbc9d3c9fc4a21e8e7ed632
Erlend Storsve <erlestor@ntnu.no>
2023-04-10 20:45:25 +0200

    Theme clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,26 @@
 import { type AppType } from "next/app"
 
 import { api } from "~/utils/api"
-
 import "~/styles/globals.css"
 import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
 import { Toaster } from "react-hot-toast"
 import Head from "next/head"
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider
+      {...pageProps}
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <Head>
         <title>Chirp</title>
-        <meta name='description' content='thinking...' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="thinking..." />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Toaster position='bottom-center' />
+      <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
   )

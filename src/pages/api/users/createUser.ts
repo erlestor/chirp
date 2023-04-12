8011b3c5d9f4417d3755aee8e831524aed600986
Erlend Storsve <erlestor@ntnu.no>
2023-04-12 16:49:52 +0200

    Test createUser

--- src/pages/api/users/createUser.ts
+++ src/pages/api/users/createUser.ts
@@ -8,13 +8,18 @@ const userByIdHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   // Create context and caller
   const ctx = createTRPCContext({ req, res })
   const caller = appRouter.createCaller(ctx)
+
   try {
     const { id, username, profilePicture } = req.query
+
+    console.log(req.query)
+
     if (typeof id !== "string") throw new Error("id must be a string")
     if (typeof username !== "string") throw new Error("username must be a string")
     if (typeof profilePicture !== "string") throw new Error("profilePicture must be a string")
-    const user = await caller.profile.createUser({ id, username, profilePicture })
-    res.status(200).json(user)
+
+    // const user = await caller.profile.createUser({ id, username, profilePicture })
+    res.status(200).json({}) // replace with user
   } catch (cause) {
     if (cause instanceof TRPCError) {
       // An error from tRPC occured

b69f5c8ca326c96ecb2ce381878e3f9d96b32d10
Erlend Storsve <erlestor@ntnu.no>
2023-04-13 17:48:08 +0200

    Validate body in createUser endpoint

--- src/pages/api/users/createUser.ts
+++ src/pages/api/users/createUser.ts
@@ -4,23 +4,24 @@ import { createTRPCContext } from "../../../server/api/trpc"
 import { TRPCError } from "@trpc/server"
 import { getHTTPStatusCodeFromError } from "@trpc/server/http"
 import { userValidator } from "~/utils/zodValidators"
+import { z } from "zod"
 
-interface IUser {
-  id: string
-  username: string
-  profilePicture: string
-}
+const bodyValidator = z.object({
+  data: userValidator,
+})
 
 const userByIdHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   // Create context and caller
   const ctx = createTRPCContext({ req, res })
   const caller = appRouter.createCaller(ctx)
 
+  // user is in req.body.data
+
   try {
-    throw new Error(JSON.stringify(req.body))
     if (req.method !== "POST") throw new Error("Method not allowed. Must be POST")
 
-    const { id, username, profilePicture } = userValidator.parse(req.body.data)
+    const body = bodyValidator.parse(req.body)
+    const { id, username, profilePicture } = body.data
 
     if (typeof id !== "string") throw new Error("id must be a string")
     if (typeof username !== "string") throw new Error("username must be a string")

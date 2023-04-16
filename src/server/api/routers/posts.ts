c5b2b47c9cdeab9d27ab4c8a8088c9a0c2821502
Erlend Storsve <erlestor@ntnu.no>
2023-04-16 14:56:07 +0200

    Separate following query

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -39,37 +39,19 @@ export const postsRouter = createTRPCRouter({
     return (await addUserDataToPosts([post]))[0]
   }),
 
-  getAll: publicProcedure.query(async ({ ctx }) => {
-    const posts = await ctx.prisma.post.findMany({
-      take: 100,
-      orderBy: {
-        createdAt: "desc",
-      },
-    })
-
-    return addUserDataToPosts(posts)
-  }),
-
-  getInfinitePosts: publicProcedure
+  getInfinite: publicProcedure
     .input(
       z.object({
         limit: z.number().min(1).max(100).nullish(),
         cursor: z.string().nullish(),
         authorId: z.string().nullish(),
-        followingOnly: z.boolean().nullish(),
       })
     )
     .query(async ({ ctx, input }) => {
       const limit = input.limit ?? 50
-      const { cursor, authorId, followingOnly } = input
+      const { cursor, authorId } = input
 
-      const authorFilter = authorId ? { authorId: authorId } : {}
-      const followFilter = followingOnly ? { following: { some: { id: ctx.userId } } } : {}
-      const filter = { ...authorFilter, ...followFilter }
-
-      // vil hent ut posts der det finnes en follow med followerId lik ctx.userId og followedId lik authorId
-      // treng da relation post n->1 user, user 2->n follow
-      // første steg blir da å sørg for at æ lagre users på egen database i tillegg te clerk
+      // const userId = ctx.userId
 
       const posts = await ctx.prisma.post.findMany({
         take: limit + 1,
@@ -77,8 +59,11 @@ export const postsRouter = createTRPCRouter({
         orderBy: {
           id: "desc",
         },
-        // filtering
-        where: authorId ? { authorId: authorId } : undefined,
+        where: authorId
+          ? { authorId: authorId }
+          : // : userId
+            // ? { NOT: { authorId: userId } }
+            undefined,
       })
 
       let nextCursor: typeof cursor | undefined = undefined
@@ -96,18 +81,53 @@ export const postsRouter = createTRPCRouter({
       }
     }),
 
-  getPostsByUserId: publicProcedure.input(z.object({ userId: z.string() })).query(
-    async ({ ctx, input }) =>
-      await ctx.prisma.post
-        .findMany({
-          where: {
-            authorId: input.userId,
+  getInfiniteFollowing: privateProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100).nullish(),
+        cursor: z.string().nullish(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      // TODO: filtrering på followingOnly
+      // hent posts der author te post e i following lista te currentUser
+
+      const limit = input.limit ?? 50
+      const { cursor } = input
+
+      const currentUserId = ctx.userId
+
+      const posts = await ctx.prisma.post.findMany({
+        take: limit + 1,
+        cursor: cursor ? { id: cursor } : undefined,
+        orderBy: {
+          id: "desc",
+        },
+        where: {
+          author: {
+            followedBy: {
+              some: {
+                followerId: currentUserId,
+              },
+            },
           },
-          take: 100,
-          orderBy: [{ createdAt: "desc" }],
-        })
-        .then(addUserDataToPosts)
-  ),
+        },
+      })
+
+      let nextCursor: typeof cursor | undefined = undefined
+
+      if (posts.length > limit) {
+        const nextPost = posts.pop()
+        nextCursor = nextPost?.id
+      }
+
+      const postsWithUser = await addUserDataToPosts(posts)
+
+      return {
+        posts: postsWithUser,
+        nextCursor,
+      }
+    }),
 
   create: privateProcedure
     .input(

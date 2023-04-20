a9a799088991a5d18389efa6cdc917adcf625f65
Erlend Storsve <erlestor@ntnu.no>
2023-04-20 18:46:02 +0200

    Fix tailwind class ordering

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 100,
+  arrowParens: 'always',
+  semi: false,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  htmlWhitespaceSensitivity: "css",
+  plugins: [require('prettier-plugin-tailwindcss')],
+}

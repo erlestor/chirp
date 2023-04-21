78717d317b305651340a651ac14a8315eedbf9e5
Erlend Storsve <erlestor@ntnu.no>
2023-04-21 23:36:14 +0200

    More readable button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,29 +1,25 @@
-import type { ReactNode } from "react"
+import type { ButtonHTMLAttributes, ReactNode } from "react"
+import { cva } from "class-variance-authority"
+import type { VariantProps } from "class-variance-authority"
 
-export const Button = (props: {
-  children: ReactNode
-  className?: string
-  onClick?: () => void
-  onMouseEnter?: () => void
-  onMouseLeave?: () => void
-  disabled?: boolean
-  contained?: boolean
-}) => {
-  const { contained, className, disabled } = props
+const button = cva(
+  "rounded-full border border-slate-400 px-4 py-1.5 text-center font-semibold disabled:brightness-50",
+  {
+    variants: {
+      contained: {
+        true: "bg-slate-100 text-black enabled:hover:brightness-90 transition",
+      },
+    },
+    defaultVariants: {
+      contained: false,
+    },
+  }
+)
 
-  return (
-    <button
-      {...props}
-      className={`
-        rounded-full border border-slate-400 px-4 py-1.5 text-center font-semibold ${
-          contained ? "bg-slate-100 text-black" : " "
-        } 
-        ${contained && !disabled ? " hover:brightness-89" : " "} 
-        ${disabled ? "brightness-50" : " "} 
-        ${className ?? " "}
-      `}
-    >
-      {props.children}
-    </button>
-  )
-}
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof button> {}
+
+export const Button = ({ className, contained, ...props }: ButtonProps) => (
+  <button className={button({ className, contained })} {...props} />
+)

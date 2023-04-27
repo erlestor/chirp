8ca4fe7f113d991938f6ba5a28e1dee238aba8f7
Erlend Storsve <erlestor@ntnu.no>
2023-04-27 04:08:27 +0200

    Style fix sidebar alignment when user logged out

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -41,7 +41,7 @@ export const Sidebar = ({
   }
 
   return (
-    <div className="md: sticky bottom-0 z-10 flex justify-around bg-white px-4 py-2 transition dark:bg-black md:top-0 md:h-screen md:flex-col md:py-4 md:pl-0">
+    <div className="md: sticky bottom-0 z-10 flex justify-around bg-white px-4 py-2 transition dark:bg-black md:top-0 md:h-screen md:flex-col md:justify-start md:py-4 md:pl-0">
       <Navlink href="/">
         <FaKiwiBird size={30} />
       </Navlink>

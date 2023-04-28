f63543c9bab60b43ab07bc3a3ca3c44ab8e776bf
Erlend Storsve <erlestor@ntnu.no>
2023-04-28 17:24:37 +0200

    Chore update openapi doc

--- src/server/api/openapi.ts
+++ src/server/api/openapi.ts
@@ -6,6 +6,6 @@ export const openApiDocument = generateOpenApiDocument(appRouter, {
   title: "Chirp OpenAPI",
   description: "OpenAPI compliant REST API built using tRPC with Next.js",
   version: "1.0.0",
-  baseUrl: "http://localhost:3000/api/test",
+  baseUrl: "http://localhost:3000/api/users/",
   tags: ["users"],
 })

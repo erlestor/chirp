f9890e4ebe39ffecd7034aef7606e46ac145e1cc
Erlend Storsve <erlestor@ntnu.no>
2023-04-29 21:15:40 +0200

    style: set darkmode on html element

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { type AppType } from "next/app"
-
 import { api } from "~/utils/api"
 import "~/styles/globals.css"
 import { ClerkProvider } from "@clerk/nextjs"
@@ -7,9 +6,18 @@ import { dark } from "@clerk/themes"
 import { Toaster } from "react-hot-toast"
 import Head from "next/head"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-import ContextProvider from "~/components/contextProvider"
+import { useEffect } from "react"
+import { DarkmodeContext, TabContext, useDarkmode, useTab } from "~/utils/context"
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  const [darkmode, setDarkmode] = useDarkmode()
+  const [tab, setTab] = useTab()
+
+  useEffect(() => {
+    if (darkmode) document.documentElement.classList.add("dark")
+    else document.documentElement.classList.remove("dark")
+  }, [darkmode])
+
   return (
     <ClerkProvider
       {...pageProps}
@@ -17,25 +25,27 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         baseTheme: dark,
       }}
     >
-      <ContextProvider>
-        <Head>
-          <title>Chirp</title>
-          <meta name="description" content="thinking..." />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Toaster
-          position="bottom-center"
-          toastOptions={{
-            style: {
-              background: "#000000",
-              color: "#ffffff",
-              fontFamily: "inter, sans",
-            },
-          }}
-        />
-        <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </ContextProvider>
+      <DarkmodeContext.Provider value={{ darkmode, setDarkmode }}>
+        <TabContext.Provider value={{ tab, setTab }}>
+          <Head>
+            <title>Chirp</title>
+            <meta name="description" content="thinking..." />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <Toaster
+            position="bottom-center"
+            toastOptions={{
+              style: {
+                background: "#000000",
+                color: "#ffffff",
+                fontFamily: "inter, sans",
+              },
+            }}
+          />
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </TabContext.Provider>
+      </DarkmodeContext.Provider>
     </ClerkProvider>
   )
 }

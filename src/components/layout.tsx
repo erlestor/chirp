659b2139735b1754571c7d004f3398e48b07e0c2
Erlend Storsve <erlestor@ntnu.no>
2023-04-29 21:14:24 +0200

    chore: simple layout variant

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,21 +1,23 @@
 /* eslint-disable react/no-unescaped-entities */
 import { SignInButton, useUser } from "@clerk/nextjs"
-import { useState, type PropsWithChildren } from "react"
+import { useState, type PropsWithChildren, useRef, useEffect } from "react"
 import { Inter } from "next/font/google"
 import { Sidebar } from "./sidebar"
-import { useDarkmodeContext } from "~/utils/context"
 
-export const inter = Inter({
+const inter = Inter({
   weight: "400",
   subsets: ["latin"],
   variable: "--font-inter",
 })
 
-export const PageLayout = (props: PropsWithChildren) => {
+interface Props extends PropsWithChildren {
+  layout?: "simple" | "default"
+}
+
+export const PageLayout = ({ children, layout = "default" }: Props) => {
   const { isLoaded, isSignedIn } = useUser()
   const [showPopover, setShowPopover] = useState(false)
-
-  const { darkmode } = useDarkmodeContext()
+  const firstRender = useRef(true)
 
   const handlePageClick = () => {
     if (showPopover) {
@@ -24,29 +26,38 @@ export const PageLayout = (props: PropsWithChildren) => {
     }
   }
 
+  useEffect(() => {
+    if (firstRender.current) firstRender.current = false
+  }, [])
+
   return (
     <main
-      className={`${inter.className} font-sans ${darkmode ? "dark" : ""}`}
+      className={`${inter.className} bg-white font-sans text-slate-900 ${
+        firstRender.current === false ? "transition duration-300" : ""
+      } dark:bg-black dark:text-slate-100`}
       onClick={handlePageClick}
     >
-      <div className="bg-white text-slate-900 transition duration-300 dark:bg-black dark:text-slate-100">
-        <div
-          className={`${
-            showPopover ? "pointer-events-none" : ""
-          } flex flex-col-reverse justify-center md:flex-row`}
-        >
-          <Sidebar showPopover={showPopover} setShowPopover={setShowPopover} />
-          <div className="min-h-screen w-full border-slate-600 md:max-w-2xl md:border-x">
-            {props.children}
+      {layout === "default" && (
+        <>
+          <div
+            className={`${
+              showPopover ? "pointer-events-none" : ""
+            } flex flex-col-reverse justify-center md:flex-row`}
+          >
+            <Sidebar showPopover={showPopover} setShowPopover={setShowPopover} />
+            <div className="min-h-screen w-full border-slate-600 md:max-w-2xl md:border-x">
+              {children}
+            </div>
           </div>
-        </div>
-        {isLoaded && !isSignedIn && (
-          <div className="fixed bottom-0 z-20 flex w-full justify-center gap-4 bg-white p-4 dark:bg-black">
-            <div className="text-xl">Don't miss what's happening</div>
-            <SignInButton />
-          </div>
-        )}
-      </div>
+          {isLoaded && !isSignedIn && (
+            <div className="fixed bottom-0 z-20 flex w-full justify-center gap-4 bg-white p-4 dark:bg-black">
+              <div className="text-xl">Don't miss what's happening</div>
+              <SignInButton />
+            </div>
+          )}
+        </>
+      )}
+      {layout === "simple" && <>{children}</>}
     </main>
   )
 }

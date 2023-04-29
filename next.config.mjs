402f28ca216da5d52d4f8876d6cd70b5f0f99d26
Erlend Storsve <erlestor@ntnu.no>
2023-04-29 17:37:15 +0200

    chore: use t3-env for checking env vars

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"))
+// !process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"))
+import "./src/env.mjs"
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -35,4 +36,5 @@ const config = {
   },
   swcMinify: true,
 }
+
 export default config
